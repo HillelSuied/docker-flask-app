78eceae303220a12f00e35a1e27f823ee76aa25a
Hillel <hillelsuied@gmail.com>
2025-02-19 12:28:44 +0200

    Updated app.py - fixed route /showcount

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+count = 0  # משתנה גלובלי לספירת ביקורים
+
 @app.route('/')
 def home():
     return "Hello, Flask is running!"
 
+@app.route('/showcount')  # צריך להיות כתוב בדיוק ככה!
+def show_count():
+    global count
+    count += 1
+    return f"Visitor count: {count}"
+
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
